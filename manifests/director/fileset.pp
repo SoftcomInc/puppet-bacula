1a5122ca32d51b271352026bfb1123d7ead9138d
root <root@bacula-sd>
2015-12-23 16:06:09 -0500

    Copy fileset.pp to director/

--- manifests/director/fileset.pp
+++ manifests/director/fileset.pp
@@ -10,9 +10,7 @@ define bacula::fileset (
   ) {
   validate_hash($options)
 
-  include bacula::common
-
-  @@concat::fragment { "bacula-fileset-${name}":
+  concat::fragment { "bacula-fileset-${name}":
     target  => "${conf_dir}/conf.d/fileset.conf",
     content => template('bacula/fileset.conf.erb'),
     tag     => "bacula-${bacula::params::director}";
